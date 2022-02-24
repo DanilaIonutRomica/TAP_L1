dabffccb7aa7ffd8ebb14caaac755bd5a3db2a83
danilaionutromica <danilaionut8@gmail.com>
2022-02-24 18:29:23 +0200

    Add project files.

---
+++ TAP_L1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What`s your name?");
+String nume=Console.ReadLine();
+Console.WriteLine($"Hello {nume}! Welcome to .NET world.");
